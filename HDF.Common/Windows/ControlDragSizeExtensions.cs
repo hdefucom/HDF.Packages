9c509237da08e6cc093ea54b3a3212ef30886e23
德芙 <1213159982@qq.com>
2021-09-16 16:00:28 +0800

    添加三个控件拓展（边框阴影、拖动位置、拖动大小）

---
+++ HDF.Common/Windows/ControlDragSizeExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HDF.Framework.Common.WinForm
+{
+    /// <summary>
+    /// 控件拖拽大小拓展
+    /// </summary>
+    public static class ControlDragSizeExtensions
+    {
+        const int WM_NCHITTEST = 0x0084;
+        const int HTLEFT = 10;      //左边界
+        const int HTRIGHT = 11;     //右边界
+        const int HTTOP = 12;       //上边界
+        const int HTTOPLEFT = 13;   //左上角
+        const int HTTOPRIGHT = 14;  //右上角
+        const int HTBOTTOM = 15;    //下边界
+        const int HTBOTTOMLEFT = 0x10;    //左下角
+        const int HTBOTTOMRIGHT = 17;     //右下角
+
+
+
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+
+        /// <summary>
+        /// 设置鼠标拖动边框修改控件大小
+        /// </summary>
+        /// <remarks>重写WndProc，调用后此方法后不可再调用base.WndProc()，否则无效</remarks>
+        /// <param name="control"></param>
+        /// <param name="m"></param>
+        /// <param name="isSetDragPostion">是否设置鼠标拖拽移动位置</param>
+        public static void SetDragSize(this Control control, ref Message m, bool isSetDragPostion = false)
+        {
+            if (control is Form form && form.FormBorderStyle != FormBorderStyle.None)
+                return;
+            switch (m.Msg)
+            {
+                case WM_NCHITTEST:
+                    //必须最后转成int16，多屏幕情况下坐标可能是负数，位运算得到结果是int16，如果使用int32，就无法使溢出的int16得到正确的坐标
+                    var vPoint = new Point(
+                        (Int16)((int)m.LParam & 0xFFFF),
+                        (Int16)((int)m.LParam >> 16 & 0xFFFF)
+                        );
+                    vPoint = control.PointToClient(vPoint);
+                    if (vPoint.X <= 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)HTTOPLEFT;
+                        else if (vPoint.Y >= control.ClientSize.Height - 5)
+                            m.Result = (IntPtr)HTBOTTOMLEFT;
+                        else m.Result = (IntPtr)HTLEFT;
+                    else if (vPoint.X >= control.ClientSize.Width - 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)HTTOPRIGHT;
+                        else if (vPoint.Y >= control.ClientSize.Height - 5)
+                            m.Result = (IntPtr)HTBOTTOMRIGHT;
+                        else m.Result = (IntPtr)HTRIGHT;
+                    else if (vPoint.Y <= 5)
+                        m.Result = (IntPtr)HTTOP;
+                    else if (vPoint.Y >= control.ClientSize.Height - 5)
+                        m.Result = (IntPtr)HTBOTTOM;
+                    else if (isSetDragPostion && (int)m.Result == HTCLIENT)
+                        m.Result = (IntPtr)HTCAPTION;
+                    break;
+            }
+
+        }
+    }
+}
---
+++ HDF.Common/Windows/ControlMovePositionExtensions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HDF.Common.Windows
+{
+    /// <summary>
+    /// 控件移动位置拓展
+    /// </summary>
+    public static class ControlMovePositionExtensions
+    {
+        /// <summary>
+        /// 绑定控件移动位置的控件，通过绑定鼠标事件实现
+        /// </summary>
+        /// <param name="control">移动位置的目标控件，例如无边框的Form</param>
+        /// <param name="controls">操控位置的控件集合，例如自定义的FormTItlePanel</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="ArgumentNullException"/>
+        public static void BindMovePositionControls(this Control control, params Control[] controls)
+        {
+            if (control == null)
+                throw new ArgumentNullException(nameof(control));
+
+            foreach (var c in controls)
+            {
+                var c1 = c;
+                while (c1.Parent != control)
+                {
+                    if (c1.Parent == null)
+                        throw new ArgumentException("绑定的控件不是目标控件的子控件", nameof(controls));
+                    c1 = c1.Parent;
+                }
+
+                BindEvent(control, c);
+            }
+        }
+
+        private readonly static Dictionary<Control, Point> dict = new();
+
+
+        private static void BindEvent(Control target, Control c)
+        {
+            var control = c;
+
+            c.MouseDown += (sender, e) =>
+            {
+                //鼠标按下时记录  点击坐标  相对 需要移动控件的客户区  的坐标
+                var p = target.PointToClient(Control.MousePosition);
+                dict[target] = new Point(-p.X, -p.Y); ;
+            };
+
+            c.MouseMove += (sender, e) =>
+            {
+                if (e.Button != MouseButtons.Left)
+                    return;
+                Point p = Control.MousePosition;
+                //鼠标移动时进行偏移 点击位置 相对 移动控件原点 的大小，
+                p.Offset(dict[target]);
+                //如果移动的是顶级窗体，直接设置屏幕坐标即可，如果是子控件，则把坐标转换成父控件的客户区坐标
+                if (target.Parent != null)
+                    p = target.Parent.PointToClient(p);
+                target.Location = p;
+            };
+        }
+
+
+
+
+
+
+        private const int WM_NCHITTEST = 0x84;          // variables for dragging the form
+        private const int HTCLIENT = 0x1;//1鼠标位置为客户区
+        private const int HTCAPTION = 0x2;//2鼠标位置为标题栏
+
+
+        /// <summary>
+        /// 设置鼠标拖动客户区移动控件位置
+        /// </summary>
+        /// <remarks>重写WndProc，调用后此方法后不可再调用base.WndProc()，否则无效</remarks>
+        /// <param name="control"></param>
+        /// <param name="m"></param>
+#pragma warning disable IDE0060 // 删除未使用的参数
+        public static void SetDragPosition(this Control control, ref Message m)
+#pragma warning restore IDE0060 // 删除未使用的参数
+        {
+            if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT)     // drag the form
+                m.Result = (IntPtr)HTCAPTION;
+        }
+
+
+
+
+
+    }
+
+
+
+}
