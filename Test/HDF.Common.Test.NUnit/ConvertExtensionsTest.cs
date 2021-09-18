d20a18331a8d1466556d98ab2d65610d2f3cd534
德芙 <1213159982@qq.com>
2021-09-18 17:34:43 +0800

    添加转换拓展单元测试

---
+++ Test/HDF.Common.Test.NUnit/ConvertExtensionsTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+
+namespace HDF.Common.Test.NUnit
+{
+    public class ConvertExtensionsTest
+    {
+
+
+        [Test]
+        public void ToTest()
+        {
+            Assert.AreEqual(1, "1".To<int>());
+            Assert.AreEqual(new DateTime(2021, 9, 18), "2021-09-18".To<DateTime>());
+            Assert.False("false".To<bool>());
+        }
+
+
+        [Test]
+        public void AsTest()
+        {
+            Assert.IsAssignableFrom<IComparable>("".As<IComparable>());
+            Assert.IsInstanceOf<Child>(((Parent)new Child()).As<Child>());
+        }
+
+        private class Parent { }
+        private class Child : Parent { }
+
+
+
+        [Test]
+        public void ToEnumTest()
+        {
+            Assert.AreEqual(Sex.Boy, "Boy".ToEnum<Sex>());
+            Assert.AreEqual(Sex.Girl, "Girl".ToEnum<Sex>());
+        }
+
+        private enum Sex { Boy, Girl }
+
+
+
+
+
+
+        [Test]
+        public void GetCustomAttributeTest()
+        {
+            Assert.NotNull(typeof(Test).GetCustomAttribute<AAttribute>());
+            Assert.NotNull(typeof(Test).GetCustomAttribute<AAttribute>(true));
+        }
+
+
+        private class AAttribute : Attribute { }
+
+        [A]
+        private class Test { }
+        private class TestChild : Test { }
+
+
+
+    }
+}
---
+++ Test/HDF.Common.Test.xUnit/ConvertExtensionsTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Linq;
+using Xunit;
+
+namespace HDF.Common.Test.xUnit
+{
+    public class ConvertExtensionsTest
+    {
+
+
+        [Fact]
+        public void ToTest()
+        {
+            Assert.Equal(1, "1".To<int>());
+            Assert.Equal(new DateTime(2021, 9, 18), "2021-09-18".To<DateTime>());
+            Assert.False("false".To<bool>());
+        }
+
+
+        [Fact]
+        public void AsTest()
+        {
+            Assert.IsAssignableFrom<IComparable>("".As<IComparable>());
+            Assert.IsType<Child>(((Parent)new Child()).As<Child>());
+        }
+
+        private class Parent { }
+        private class Child : Parent { }
+
+
+
+        [Fact]
+        public void ToEnumTest()
+        {
+            Assert.Equal(Sex.Boy, "Boy".ToEnum<Sex>());
+            Assert.Equal(Sex.Girl, "Girl".ToEnum<Sex>());
+        }
+
+        private enum Sex { Boy, Girl }
+
+
+
+
+
+
+        [Fact]
+        public void GetCustomAttributeTest()
+        {
+            Assert.NotNull(typeof(Test).GetCustomAttribute<AAttribute>());
+            Assert.NotNull(typeof(Test).GetCustomAttribute<AAttribute>(true));
+        }
+
+
+        private class AAttribute : Attribute { }
+
+        [A]
+        private class Test { }
+        private class TestChild : Test { }
+
+
+
+    }
+}
