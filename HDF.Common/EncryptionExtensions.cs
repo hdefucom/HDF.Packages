42e3247d0f1fa7b23e550b3214a976b750f6f47d
德芙 <1213159982@qq.com>
2021-09-18 10:45:09 +0800

    优化MD5加密拓展

--- HDF.Common/EncryptionExtensions.cs
+++ HDF.Common/EncryptionExtensions.cs
@@ -13,18 +13,23 @@ namespace HDF.Common
         /// 将传入的字符串以MD5的方式加密
         /// </summary>
         /// <param name="value">要加密的字符串</param>
+        /// <param name="is32">是否返回32位，默认true，false则返回16位</param>
+        /// <param name="isUpper">是否返回大写字符，默认true，false则返回小写字符</param>
         /// <returns>加密后的字符串</returns>
-        public static string ToMD5(this string value)
+        public static string ToMD5(this string value, bool is32 = true, bool isUpper = true)
         {
             if (value.IsNullOrEmpty())
                 return string.Empty;
 
             using MD5 md5 = new MD5CryptoServiceProvider();
-            byte[] bytes = Encoding.Default.GetBytes(value);//将要加密的字符串转换为字节数组
-            byte[] encryptdata = md5.ComputeHash(bytes);//将字符串加密后也转换为字符数组
-            return Convert.ToBase64String(encryptdata);//将加密后的字节数组转换为加密字符串
+            byte[] bytes = Encoding.Default.GetBytes(value);
+            byte[] encryptdata = md5.ComputeHash(bytes);
+            var res = is32 ? BitConverter.ToString(encryptdata) : BitConverter.ToString(encryptdata, 4, 8);
+            res = res.Replace("-", "");
+            return isUpper ? res : res.ToLower();
         }
 
     }
+
 
 }
