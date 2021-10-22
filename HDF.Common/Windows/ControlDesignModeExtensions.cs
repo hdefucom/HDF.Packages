8d4c4cb20c31a7833f7c2e9a32217271436e8a69
德芙 <1213159982@qq.com>
2021-10-22 09:30:36 +0800

    添加控件设计模式状态拓展

---
+++ HDF.Common/Windows/ControlDesignModeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace HDF.Common.Windows
+{
+    /// <summary>
+    /// 控件设计模式状态拓展
+    /// </summary>
+    public static class ControlDesignModeExtensions
+    {
+
+
+        /// <summary>
+        /// 判断该控件是否处于设计器模式
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static bool IsDesignMode(this Control c) => c.Site.DesignMode || LicenseManager.UsageMode == LicenseUsageMode.Designtime;
+
+
+
+
+
+    }
+}
