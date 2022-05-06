2bac3b07d605e736811b5d8e11a3361c2b1c17d4
德芙 <1213159982@qq.com>
2022-05-06 15:32:59 +0800

    init特性

--- HDF.DotNet.Features.PropertyInitAccessor/IsExternalInit.cs
+++ HDF.DotNet.Features.PropertyInitAccessor/IsExternalInit.cs
@@ -1,15 +1,16 @@
 ﻿
-#if !NET5_0_OR_GREATER
+#if NET5_0_OR_GREATER
 
+using System.Runtime.CompilerServices;
+[assembly: TypeForwardedTo(typeof(IsExternalInit))]
 
+#else
 using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
-#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
     public class IsExternalInit { }
-#pragma warning restore CS1591 // 缺少对公共可见类型或成员的 XML 注释
 }
 
 #endif
