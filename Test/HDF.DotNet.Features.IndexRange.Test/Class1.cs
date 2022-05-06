053fab019268ef28f07af5d1e1a14b8c0fe02e12
德芙 <1213159982@qq.com>
2022-05-06 15:33:25 +0800

    测试用例

---
+++ Test/HDF.DotNet.Features.IndexRange.Test/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace HDF.DotNet.Features.IndexRange.Test
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+            var array = new int[10];
+
+            var a = ^1;
+
+            var b = 1..2;
+
+            var c = array[a];
+            var d = array[b];
+
+
+        }
+    }
+}
---
+++ Test/HDF.DotNet.Features.PropertyInitAccessor.Test/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace HDF.DotNet.Features.PropertyInitAccessor.Test
+{
+    internal class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+
+        public int MyProperty { get; init; }
+
+    }
+
+    public record TestRecord(string str);
+}
