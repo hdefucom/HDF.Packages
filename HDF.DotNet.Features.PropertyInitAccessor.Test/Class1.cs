554e681884a81ed537aa9f03975dcb4279e8fbf0
德芙 <1213159982@qq.com>
2022-05-06 14:40:49 +0800

    添加Net5init特性拓展包，给低版本.net提供init功能

---
+++ HDF.DotNet.Features.PropertyInitAccessor.Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace HDF.DotNet.Features.PropertyInitAccessor.Test
+{
+    internal class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+
+        public int MyProperty { get; init; }
+
+    }
+}
---
+++ HDF.DotNet.Features.PropertyInitAccessor/IsExternalInit.cs
@@ -0,0 +1,15 @@
+﻿
+#if !NET5_0_OR_GREATER
+
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+#pragma warning disable CS1591 // 缺少对公共可见类型或成员的 XML 注释
+    public class IsExternalInit { }
+#pragma warning restore CS1591 // 缺少对公共可见类型或成员的 XML 注释
+}
+
+#endif
